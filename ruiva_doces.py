b3cc0fda184ef0e096c854a8c4173a8cee308769
Kiabo <109933846+andrekiabo@users.noreply.github.com>
2024-01-14 02:47:48 -0300

    Update ruiva_doces.py

--- ruiva_doces.py
+++ ruiva_doces.py
@@ -35,15 +35,14 @@ resultado_ninho_com_nutella = 0
 condicao = True
 while condicao: 
     print('')
-    print('Para somar tudo digite: fim')
     tipo = input('Digite o tipo de doce: ')
-    if tipo == 'fim':
-        print('')
-        print('A soma dos resultados é:',resultado_limao + resultado_ninho_trufado_com_nutella + resultado_geleia_de_morango + resultado_maracuja + resultado_pistache + resultado_physalis + resultado_brigadeiro_gourmet + resultado_caramelo_salgado + resultado_surpresa_de_uva + resultado_cereja + resultado_camafeu_de_nozes + resultado_creme_brulee_com_nozes + resultado_ganache_branco_com_morango + resultado_ganache_ao_leite_com_praline + resultado_trufa_meio_amarga + resultado_frutas_vermelhas + resultado_trufa_de_coco + resultado_beijinho + resultado_leite_ninho + resultado_amendoim + resultado_bicho_de_pe_morango + resultado_casadinho + resultado_tradicional_com_amendoim + resultado_olho_de_sogra_ameixa + resultado_damasco + resultado_creme_brulee + resultado_tradicional + resultado_churros + resultado_tradicional_dourado + resultado_ninho_com_nutella
-        )
-        break
+    # if tipo == 'fim':
 
     quantos = input('Digite quantas unidades: ')
+    print('')
+
+    # print('A soma dos resultados é:',resultado_limao + resultado_ninho_trufado_com_nutella + resultado_geleia_de_morango + resultado_maracuja + resultado_pistache + resultado_physalis + resultado_brigadeiro_gourmet + resultado_caramelo_salgado + resultado_surpresa_de_uva + resultado_cereja + resultado_camafeu_de_nozes + resultado_creme_brulee_com_nozes + resultado_ganache_branco_com_morango + resultado_ganache_ao_leite_com_praline + resultado_trufa_meio_amarga + resultado_frutas_vermelhas + resultado_trufa_de_coco + resultado_beijinho + resultado_leite_ninho + resultado_amendoim + resultado_bicho_de_pe_morango + resultado_casadinho + resultado_tradicional_com_amendoim + resultado_olho_de_sogra_ameixa + resultado_damasco + resultado_creme_brulee + resultado_tradicional + resultado_churros + resultado_tradicional_dourado + resultado_ninho_com_nutella
+    # )
     
     try:
         quantos = int(quantos)
@@ -85,155 +84,160 @@ while condicao:
     ninho_com_nutella = float(190)
 
     if tipo == 'limao':
-        print(f'{tipo} - R${limao:.2f}')
+        print(f'- {tipo} R${limao:.2f}')
         resultado_limao = (limao / 100) * quantos
-        print(f'{quantos} unidade(s) de {tipo}: R${resultado_limao:.2f}')
+        print(f'- {quantos} unidade(s) de {tipo}: R${resultado_limao:.2f}')
 
     elif tipo == 'ninho trufado com nutella':
-        print(f'{tipo} - R${ninho_trufado_com_nutella:.2f}')
+        print(f'- {tipo} R${ninho_trufado_com_nutella:.2f}')
         resultado_ninho_trufado_com_nutella = (ninho_trufado_com_nutella / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_ninho_trufado_com_nutella:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_ninho_trufado_com_nutella:.2f}')
 
     elif tipo == 'geleia de morango':
-        print(f'{tipo} - R${geleia_de_morango:.2f}')
+        print(f'- {tipo} R${geleia_de_morango:.2f}')
         resultado_geleia_de_morango = (geleia_de_morango / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_geleia_de_morango:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_geleia_de_morango:.2f}')
 
     elif tipo == 'maracuja':
-        print(f'{tipo} - R${maracuja:.2f}')
+        print(f'- {tipo} R${maracuja:.2f}')
         resultado_maracuja = (maracuja / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_maracuja:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_maracuja:.2f}')
 
     elif tipo == 'pistache':
-        print(f'{tipo} - R${pistache:.2f}')
+        print(f'- {tipo} R${pistache:.2f}')
         resultado_pistache = (pistache / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_pistache:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_pistache:.2f}')
 
     elif tipo == 'physalis':
-        print(f'{tipo} - R${physalis:.2f}')
+        print(f'- {tipo} R${physalis:.2f}')
         resultado_physalis = (physalis / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_physalis:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_physalis:.2f}')
 
     elif tipo == 'brigadeiro gourmet':
-        print(f'{tipo} - R${brigadeiro_gourmet:.2f}')
+        print(f'- {tipo} R${brigadeiro_gourmet:.2f}')
         resultado_brigadeiro_gourmet = (brigadeiro_gourmet / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_brigadeiro_gourmet:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_brigadeiro_gourmet:.2f}')
 
     elif tipo == 'caramelo salgado':
-        print(f'{tipo} - R${caramelo_salgado:.2f}')
+        print(f'- {tipo} R${caramelo_salgado:.2f}')
         resultado_caramelo_salgado = (caramelo_salgado / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_caramelo_salgado:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_caramelo_salgado:.2f}')
 
     elif tipo == 'surpresa de uva':
-        print(f'{tipo} - R${surpresa_de_uva:.2f}')
+        print(f'- {tipo} R${surpresa_de_uva:.2f}')
         resultado_surpresa_de_uva = (surpresa_de_uva / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_surpresa_de_uva:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_surpresa_de_uva:.2f}')
 
     elif tipo == 'cereja':
-        print(f'{tipo} - R${cereja:.2f}')
+        print(f'- {tipo} R${cereja:.2f}')
         resultado_cereja = (cereja / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_cereja:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_cereja:.2f}')
 
     elif tipo == 'camafeu de nozes':
-        print(f'{tipo} - R${camafeu_de_nozes:.2f}')
+        print(f'- {tipo} R${camafeu_de_nozes:.2f}')
         resultado_camafeu_de_nozes = (camafeu_de_nozes / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_camafeu_de_nozes:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_camafeu_de_nozes:.2f}')
 
     elif tipo == 'creme brulee com nozes':
-        print(f'{tipo} - R${creme_brulee_com_nozes:.2f}')
+        print(f'- {tipo} R${creme_brulee_com_nozes:.2f}')
         resultado_creme_brulee_com_nozes = (creme_brulee_com_nozes / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_creme_brulee_com_nozes:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_creme_brulee_com_nozes:.2f}')
 
     elif tipo == 'ganache branco c/ morango':
-        print(f'{tipo} - R${ganache_branco_com_morango:.2f}')
+        print(f'- {tipo} R${ganache_branco_com_morango:.2f}')
         resultado_ganache_branco_com_morango = (ganache_branco_com_morango / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_ganache_branco_com_morango:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_ganache_branco_com_morango:.2f}')
 
     elif tipo == 'ganache ao leite com praline':
-        print(f'{tipo} - R${ganache_ao_leite_com_praline:.2f}')
+        print(f'- {tipo} R${ganache_ao_leite_com_praline:.2f}')
         resultado_ganache_ao_leite_com_praline = (ganache_ao_leite_com_praline / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_ganache_ao_leite_com_praline:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_ganache_ao_leite_com_praline:.2f}')
 
     elif tipo == 'trufa meio amarga':
-        print(f'{tipo} - R${trufa_meio_amarga:.2f}')
+        print(f'- {tipo} R${trufa_meio_amarga:.2f}')
         resultado_trufa_meio_amarga = (trufa_meio_amarga / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_trufa_meio_amarga:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_trufa_meio_amarga:.2f}')
 
     elif tipo == 'frutas vermelhas':
-        print(f'{tipo} - R${frutas_vermelhas:.2f}')
+        print(f'- {tipo} R${frutas_vermelhas:.2f}')
         resultado_frutas_vermelhas = (frutas_vermelhas / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_frutas_vermelhas:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_frutas_vermelhas:.2f}')
 
     elif tipo == 'trufa de coco':
-        print(f'{tipo} - R${trufa_de_coco:.2f}')
+        print(f'- {tipo} R${trufa_de_coco:.2f}')
         resultado_trufa_de_coco = (trufa_de_coco / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_trufa_de_coco:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_trufa_de_coco:.2f}')
 
     elif tipo == 'beijinho':
-        print(f'{tipo} - R${beijinho:.2f}')
+        print(f'- {tipo} R${beijinho:.2f}')
         resultado_beijinho = (beijinho / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_beijinho:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_beijinho:.2f}')
 
     elif tipo == 'leite ninho':
-        print(f'{tipo} - R${leite_ninho:.2f}')
+        print(f'- {tipo} R${leite_ninho:.2f}')
         resultado_leite_ninho = (leite_ninho / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_leite_ninho:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_leite_ninho:.2f}')
 
     elif tipo == 'amendoim':
-        print(f'{tipo} - R${amendoim:.2f}')
+        print(f'- {tipo} R${amendoim:.2f}')
         resultado_amendoim = (amendoim / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_amendoim:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_amendoim:.2f}')
 
     elif tipo == 'bicho de pe morango':
-        print(f'{tipo} - R${bicho_de_pe_morango:.2f}')
+        print(f'- {tipo} R${bicho_de_pe_morango:.2f}')
         resultado_bicho_de_pe_morango = (bicho_de_pe_morango / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_bicho_de_pe_morango:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_bicho_de_pe_morango:.2f}')
 
     elif tipo == 'casadinho':
-        print(f'{tipo} - R${casadinho:.2f}')
+        print(f'- {tipo} R${casadinho:.2f}')
         resultado_casadinho = (casadinho / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_casadinho:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_casadinho:.2f}')
 
     elif tipo == 'tradicional com amendoim':
-        print(f'{tipo} - R${tradicional_com_amendoim:.2f}')
+        print(f'- {tipo} R${tradicional_com_amendoim:.2f}')
         resultado_tradicional_com_amendoim = (tradicional_com_amendoim / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_tradicional_com_amendoim:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_tradicional_com_amendoim:.2f}')
 
     elif tipo == 'olho de sogra ameixa':
-        print(f'{tipo} - R${olho_de_sogra_ameixa:.2f}')
+        print(f'- {tipo} R${olho_de_sogra_ameixa:.2f}')
         resultado_olho_de_sogra_ameixa = (olho_de_sogra_ameixa / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_olho_de_sogra_ameixa:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_olho_de_sogra_ameixa:.2f}')
 
     elif tipo == 'damasco':
-        print(f'{tipo} - R${damasco:.2f}')
+        print(f'- {tipo} R${damasco:.2f}')
         resultado_damasco = (damasco / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_damasco:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_damasco:.2f}')
 
     elif tipo == 'creme brulee':
-        print(f'{tipo} - R${creme_brulee:.2f}')
+        print(f'- {tipo} R${creme_brulee:.2f}')
         resultado_creme_brulee = (creme_brulee / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_creme_brulee:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_creme_brulee:.2f}')
 
     elif tipo == 'tradicional':
-        print(f'{tipo} - R${tradicional:.2f}')
+        print(f'- {tipo} R${tradicional:.2f}')
         resultado_tradicional = (tradicional / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_tradicional:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_tradicional:.2f}')
 
     elif tipo == 'churros':
-        print(f'{tipo} - R${churros:.2f}')
+        print(f'- {tipo} R${churros:.2f}')
         resultado_churros = (churros / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_churros:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_churros:.2f}')
 
     elif tipo == 'tradicional dourado':
-        print(f'{tipo} - R${tradicional_dourado:.2f}')
+        print(f'- {tipo} R${tradicional_dourado:.2f}')
         resultado_tradicional_dourado = (tradicional_dourado / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_tradicional_dourado:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_tradicional_dourado:.2f}')
 
     elif tipo == 'ninho com nutella':
-        print(f'{tipo} - R${ninho_com_nutella:.2f}')
+        print(f'- {tipo} R${ninho_com_nutella:.2f}')
         resultado_ninho_com_nutella = (ninho_com_nutella / 100) * quantos
-        print(f'{quantos} unidades de {tipo}: R${resultado_ninho_com_nutella:.2f}')
+        print(f'- {quantos} unidades de {tipo}: R${resultado_ninho_com_nutella:.2f}')
+    
 
     else:
         print('Doce não encontrado, tente novamente.')
+
+    if quantos >= 0:
+        valor_total = resultado_limao + resultado_ninho_trufado_com_nutella + resultado_geleia_de_morango + resultado_maracuja + resultado_pistache + resultado_physalis + resultado_brigadeiro_gourmet + resultado_caramelo_salgado + resultado_surpresa_de_uva + resultado_cereja + resultado_camafeu_de_nozes + resultado_creme_brulee_com_nozes + resultado_ganache_branco_com_morango + resultado_ganache_ao_leite_com_praline + resultado_trufa_meio_amarga + resultado_frutas_vermelhas + resultado_trufa_de_coco + resultado_beijinho + resultado_leite_ninho + resultado_amendoim + resultado_bicho_de_pe_morango + resultado_casadinho + resultado_tradicional_com_amendoim + resultado_olho_de_sogra_ameixa + resultado_damasco + resultado_creme_brulee + resultado_tradicional + resultado_churros + resultado_tradicional_dourado + resultado_ninho_com_nutella
+        print(f'- A soma total dos resultados até agora é: {valor_total:.2f}')
 
